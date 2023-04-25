8c3f2f8cefb91d9561ae219ccbe05b9f990c9c75
yunji jo <dbtmxlsk2007@naver.com>
2023-04-25 21:08:05 +0900

    second commit

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 # 추가로 등록할 수 있는 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('생성일', {'fields': ['pub_data'], 'classes':['collapse']}),
+        ('질문 섹션', {'fields': ['question_text']})
+    ]
+    list_display = ('question_text', 'pub_data', 'was_published_recently')
+    readonly_fields = ['pub_data']
+    inlines = [ChoiceInline]
+    list_filter = ['pub_data']
+    search_fields = ['question_text', 'choice__choice_text']
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 import datetime
+
 
 # 모델 생성
 # 모델을 테이블에 써 주기 위한 마이그레이션이라는 것을 만든다. 
@@ -12,12 +14,10 @@ import datetime
 # 도심 호캉스
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200) # 질문내용
-    pub_data = models.DateTimeField(auto_now_add=True) # 생성날짜
-    # is_something_wrong = models.BooleanField(default=False)
-    # score = models.FloatField(default=0.0)
-    # json_field = models.JSONField(default=dict)
+    question_text = models.CharField(max_length=200, verbose_name ='질문') # 질문내용
+    pub_data = models.DateTimeField(auto_now_add=True, verbose_name ='생성일') # 생성날짜
     
+    @admin.display(boolean = True, description='최근 생성(하루기준)')
     def was_published_recently(self):
         return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
 
